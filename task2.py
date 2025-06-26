43b2503bf829b606a90eb43c906bb77c7d4baa49
dev45688 <dev.connect45@gmail.com>
2025-06-26 23:05:01 +0530

    Update task2.py

--- task2.py
+++ task2.py
@@ -1,6 +1,6 @@
 # module2:
 #  task2:
-a = input("enter your first name:")
-b = input("enter your last name:")
-full_name= a+' '+b
+first_name = input("enter your first name:")
+last_name = input("enter your last name:")
+full_name= first_name +' '+last_name
 print('Hello,',full_name,'! Welcome to the Python program')
